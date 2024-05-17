13e14d29108a13214af4e5a1229268730da192d6
Hakob <hakobeg@gmail.com>
2024-05-17 13:53:25 +0400

    Create and run the server

---
+++ api/README.md
@@ -0,0 +1,29 @@
+# blog-app api
+
+## Available Scripts
+
+In the project directory, you can run:
+### `cd api`
+### `npm run dev`
+
+npx express-generator --no-view
+
+- Additional packages
+
+```
+npm i express
+npm i nodemon
+npm i dotenv
+npm i mongoose  
+npm i winston
+npm i moment
+npm i node-cron
+npm i express-fileupload
+npm i bcryptjs  
+npm i jsonwebtoken  
+npm i cookie-parser
+npm i cors
+npm i express-session
+npm i express-fileupload
+
+```
---
+++ api/app.js
@@ -0,0 +1,66 @@
+import express from "express"
+import path from "path"
+import { fileURLToPath } from "url"
+import dotenv from "dotenv"
+import cors from "cors"
+import fileUpload from "express-fileupload"
+import cookieParser from "cookie-parser"
+import logger from "morgan"
+
+const __filename = fileURLToPath(import.meta.url) // Get current file path
+const __dirname = path.dirname(__filename) // Derive directory path
+
+const app = express()
+// Handle requests for favicon.ico
+app.get("/favicon.ico", (req, res) => res.status(204))
+dotenv.config()
+
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+// Set CORS options
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:8800",
+]
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+//   origin:"*",
+  credentials: true, // Allow sending cookies from client to server
+}
+app.use(cors(corsOptions))
+
+//Fileupload
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    // useTempFiles : true,
+    // tempFileDir : __dirname + '/tmp',
+    // safeFileNames: true,
+    // preserveExtension: true,
+  })
+)
+
+app.use(express.static(path.join(__dirname, "public")))
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500
+  const errorMessage = err.message || "Something went wrong!"
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  })
+})
+export default app
