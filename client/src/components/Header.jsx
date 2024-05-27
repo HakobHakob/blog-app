ca70afabbf7df88398d5b59d48982306946be759
Hakob <hakobeg@gmail.com>
2024-05-28 00:02:21 +0400

    (Update):Header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,13 @@
-import { Button, Navbar,  TextInput } from "flowbite-react"
-import { Link,useLocation } from "react-router-dom"
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
+import { Link, useLocation } from "react-router-dom"
 import { AiOutlineSearch } from "react-icons/ai"
 import { FaMoon } from "react-icons/fa"
+import { useSelector } from "react-redux"
+
 export const Header = () => {
-    const path = useLocation().pathname
+  const path = useLocation().pathname
+  const { currentUser } = useSelector((state) => state.user)
+  console.log("currentUser", currentUser)
   return (
     <Navbar className="border-b-2">
       <Link
@@ -28,35 +32,52 @@ export const Header = () => {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex gap-2 md:order-2" >
+      <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
-        <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
-        </Link>
-      <Navbar.Toggle/>
-      </div>
-        {/**Burger menu */}
-       <Navbar.Collapse>
-        <Navbar.Link active={path === "/"} as={"div"}>
-            <Link to="/">
-            Home
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt="user" img={currentUser.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className="block text-sm "> @{currentUser.username}</span>
+              <span className="block text-sm font-medium truncate">              
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+             <Link to="/dashboard?tab=profile">
+              <Dropdown.Item>Profile</Dropdown.Item>
             </Link>
+            <Dropdown.Divider/>
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to="/sign-in">
+            <Button gradientDuoTone="purpleToBlue" outline>
+              Sign In
+            </Button>
+          </Link>
+        )}
+
+        <Navbar.Toggle />
+      </div>
+      {/**Burger menu */}
+      <Navbar.Collapse>
+        <Navbar.Link active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/about"} as={"div"}>
-            <Link to="/about">
-            About
-            </Link>
+          <Link to="/about">About</Link>
         </Navbar.Link>
         <Navbar.Link active={path === "/projects"} as={"div"}>
-            <Link to="/projects">
-            Projects
-            </Link>
+          <Link to="/projects">Projects</Link>
         </Navbar.Link>
-       </Navbar.Collapse>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
