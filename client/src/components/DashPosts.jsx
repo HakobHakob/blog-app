857d0967ed633614b15fd678420940123dde3fb0
Hakob <hakobeg@gmail.com>
2024-06-25 11:21:37 +0400

    (Add): show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom"
 export const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user)
   const [userPosts, setUserPosts] = useState([])
+  const [showMore, setShowMore] = useState(true)
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -17,6 +18,9 @@ export const DashPosts = () => {
 
         if (res.ok) {
           setUserPosts(data.posts)
+          if (data.posts.length < 9) {
+            setShowMore(false)
+          }
         }
       } catch (error) {
         console.log(error.message)
@@ -26,8 +30,26 @@ export const DashPosts = () => {
       fetchPosts()
     }
   }, [currentUser._id, currentUser.isAdmin])
+
+  const showMorePosts = async () => {
+    const startIndex = userPosts.length
+    try {
+      const res = await fetch(
+        `/api_v1/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      )
+      const data = await res.json()
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts])
+        if (data.posts.length < 9) {
+          setShowMore(false)
+        }
+      }
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
   return (
-    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700  dark:scrollbar-thumb-slate-500">
+    <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
         <>
           <Table hoverable className="shadow-md">
@@ -82,6 +104,14 @@ export const DashPosts = () => {
               </Table.Body>
             ))}
           </Table>
+          {showMore && (
+            <button
+              onClick={showMorePosts}
+              className="w-full text-teal-500 self-center text-sm py-7"
+            >
+              Show more
+            </button>
+          )}
         </>
       ) : (
         <p>You have no posts yet!</p>
